57870e6c72127b4ba825e0b47ca9708f5442518b
Paul Kompfner <kompfner@gmail.com>
2020-03-16 15:45:16 -0400

    Demo app using React and the CallObject API

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname, cacheControl: false });
 });
 
+// Static demos - match /static-demos
+
 app.use(
   "/static-demos",
   createProxyMiddleware({
     target: "http://localhost:3001"
+  })
+);
+
+// React demo - match /react-demo
+
+app.use(
+  "/react-demo",
+  createProxyMiddleware({
+    target: "http://localhost:3002"
   })
 );
 
